ede70d637e3a0e3901bca6333a76c5e9c4a8f3a3
JCSCMaster <jmcmaster@email.arizona.edu>
2019-09-17 16:49:09 -0700

    Init code

---
+++ src/view/Gamejam.java
@@ -0,0 +1,15 @@
+package view;
+/**
+ * Implementation of the Gamejam project.
+ * Acts as the Init class.
+ * @author Joey McMaster
+ * @author Linjie liu
+ * @author Nicholas Fiegel
+ * @author Wes Rodgers
+ *
+ */
+public class Gamejam {
+	public static void main(String args[]) {
+		
+	}
+}
