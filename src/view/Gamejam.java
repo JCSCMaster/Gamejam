339aecfb51a4d83b8bdbf5e4345f631b03e861b9
JCSCMaster <jmcmaster@email.arizona.edu>
2019-09-19 16:44:22 -0700

    Added gradle and got a base application to laod

--- src/view/Gamejam.java
+++ src/view/Gamejam.java
@@ -1,5 +1,8 @@
 package view;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 /**
  * Implementation of the Gamejam project.
  * Acts as the Init class.
@@ -9,8 +12,17 @@ import javafx.application.Application;
  * @author Wes Rodgers
  *
  */
-public class Gamejam {
+public class Gamejam extends Application {
 	public static void main(String args[]) {
-		
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("Game Jam");
+		BorderPane basepane = new BorderPane();
+		Scene scene = new Scene(basepane, 1000, 600);
+		stage.setScene(scene);
+		stage.show();
 	}
 }
