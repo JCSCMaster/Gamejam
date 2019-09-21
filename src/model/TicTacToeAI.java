db51fe4c676edc0c62477b16f660b0db35a8ff8f
WesRodgers <wesrod@gmail.com>
2019-09-21 12:26:44 -0700

    TicTacToeAI created
    Basically a stub class until I know exactly what linjie is calling his
    strategy classes and how to get nextMove from them.

--- src/model/TicTacToeAI.java
+++ src/model/TicTacToeAI.java
@@ -2,15 +2,40 @@ package model;
 
 import java.awt.Point;
 
+/**
+ * This class lets a person choose from a variety of different AIs to play
+ * against in Tic Tac Toe. Dependent on TODO insert tictactoe strategy name
+ * 
+ * @author Wes Rodgers
+ *
+ */
+
 public class TicTacToeAI {
 
-    public void setStrategy() {
-        // TODO Auto-generated method stub
-        
+    // TODO insert tictactoestrategy field
+
+    /**
+     * Constructor for the TicTacToeAI
+     */
+    public TicTacToeAI() {
+        // TODO strategy = new strategy();
     }
 
+    /**
+     * Setter for strategy type
+     */
+    public void setStrategy(/* TODO insert strategy here */) {
+        // TODO strategy = parameterStrategy;
+    }
+
+    /**
+     * Asks the strategy what the next move it would make is.
+     * 
+     * @param ticTacToeModel the current state of the tictactoe game
+     * @return a java.awt.Point storing an x and y (row and col) value
+     */
     public Point nextMove(TicTacToeModel ticTacToeModel) {
-        // TODO Auto-generated method stub
+        // TODO return strategy.nextMove(ticTacToeModel);
         return null;
     }
 
