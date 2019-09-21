6e57a5648a67e19adbf2f005cb521f7edb8e98cf
WesRodgers <wesrod@gmail.com>
2019-09-21 12:10:10 -0700

    TicTacToeModel fleshed out

--- src/model/TicTacToeAI.java
+++ src/model/TicTacToeAI.java
@@ -1,10 +1,17 @@
 package model;
 
+import java.awt.Point;
+
 public class TicTacToeAI {
 
     public void setStrategy() {
         // TODO Auto-generated method stub
         
+    }
+
+    public Point nextMove(TicTacToeModel ticTacToeModel) {
+        // TODO Auto-generated method stub
+        return null;
     }
 
 }
