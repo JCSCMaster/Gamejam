2c4ea67ba12f5f307534068b0c8fadb91e1f4dd0
WesRodgers <wesrod@gmail.com>
2019-09-21 14:28:18 -0700

    Preparing for junit tests
    I added the source directory for the junit test cases to the gradle
    build file, removed an unused import from the controllerview and created
    a junit test stub for the tictactoe mvc components' unit tests.

--- src/view/TicTacToeControllerView.java
+++ src/view/TicTacToeControllerView.java
@@ -7,7 +7,6 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import model.TicTacToeModel;
 
 /**
